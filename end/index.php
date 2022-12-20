03623ed534b18cd9228bf7772ccfc065983e75f2
NLAKHDARI <nagib.lakhdari@ynov.com>
2022-12-20 01:11:20 +0100

    php pages

--- end/index.php
+++ end/index.php
@@ -1,7 +1,19 @@
 <?php
     include_once('./view/_header.php');
-    include_once('./view/home.php');
+
+    $request = "$_SERVER[REQUEST_URI]";
+    
+    if($request == '/login'){
+        include_once('./view/login/login.php');
+    }else{
+        include_once('./view/home.php');
+
+        echo "$_SERVER[REQUEST_URI]";
+    };
     include_once('./view/_footer.php');
     include_once('./view/_script.php');
     include_once('./view/_end-page.php');
+
+     
+
 ?>
---
+++ end/view/login/login.php
@@ -0,0 +1,38 @@
+<body>
+    <div class="top-buffer">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h1 class="text-center">Login</h1>
+                                <hr />
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <form action="login.php" method="post">
+                                    <div class="form-group">
+                                        <label for="username">Username</label>
+                                        <input type="text" class="form-control" name="username" id="username" placeholder="Username" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="password">Password</label>
+                                        <input type="password" class="form-control" name="password" id="password" placeholder="Password" />
+                                    </div>
+                                    <div class="form-group text-center">
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
