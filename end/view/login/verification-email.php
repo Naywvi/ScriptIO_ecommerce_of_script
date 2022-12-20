5a3777c2d2f02d2b2629c4346c9f26d60b77d8ac
NLAKHDARI <nagib.lakhdari@ynov.com>
2022-12-20 11:40:09 +0100

    user pages

---
+++ end/view/login/verification-email.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ScriptiO - Email Confirmation</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="./assets/css/link-verification-email.css" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+
+<body>
+    <!-- start preheader -->
+    <div class="container-fluid reset-space">
+    <div class="preheader">
+        A preheader is the short summary text that follows the subject line when an email is viewed in the inbox.
+    </div>
+    <!-- end preheader -->
+        <!-- start body -->
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+            <!-- start logo -->
+            <tr>
+                <td align="center" bgcolor="#e9ecef">
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" class="table">
+                    <tr>
+                        <td align="center" valign="top" class="table-td">
+                        <a href="#" target="_blank">
+                            <img src="./assets/img/Scriptio.png" alt="Logo" border="0" width="60" class="logo-email">
+                        </a>
+                        </td>
+                    </tr>
+                    </table>
+                </td>
+            </tr>
+            <!-- end logo -->
+            <!-- start hero -->
+            <tr>
+                <td align="center" bgcolor="#e9ecef">
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" class="table">
+                    <tr>
+                        <td align="left" bgcolor="#ffffff" class="confirm-email">
+                        <h1 class="confirm-email-title">Confirm Your Email Address</h1>
+                        </td>
+                    </tr>
+                    </table>
+                </td>
+            </tr>
+            <!-- end hero -->
+            <!-- start copy block -->
+            <tr>
+                <td align="center" bgcolor="#e9ecef">
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" class="table">
+                    <!-- start copy -->
+                    <tr>
+                        <td align="left" bgcolor="#ffffff" class="table-td-p">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been <a href="#">the industry's</a>, standard dummy text ever.</p>
+                        </td>
+                    </tr>
+                    <!-- end copy -->
+                    <!-- start button -->
+                    <tr>
+                        <td align="left" bgcolor="#ffffff">
+                        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                            <tr>
+                                <td align="center" bgcolor="#ffffff" class="table-12">
+                                    <table border="0" cellpadding="0" cellspacing="0">
+                                    <tr>
+                                        <td align="center" bgcolor="#1a82e2" class="table-12-td">
+                                            <a href="#" target="_blank">Do Something Sweet</a>
+                                        </td>
+                                    </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                        </td>
+                    </tr>
+                    <!-- end button -->
+                    <!-- start copy -->
+                    <tr>
+                        <td align="left" bgcolor="#ffffff" class="table-td-p">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry:</p>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
+                        </td>
+                    </tr>
+                    <!-- end copy -->
+                    <!-- start copy -->
+                    <tr>
+                        <td align="left" bgcolor="#ffffff" class="">
+                            <p>Cheers,<br> Paste</p>
+                        </td>
+                    </tr>
+                    <!-- end copy -->
+                    </table>
+                </td>
+            </tr>
+            <!-- end copy block -->
+            <!-- start footer -->
+            <tr>
+                <td align="center" bgcolor="#e9ecef" class="td-24">
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" class="table">
+                    <!-- start permission -->
+                    <tr>
+                        <td align="center" bgcolor="#e9ecef" class="table-24-color">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                        </td>
+                    </tr>
+                    <!-- end permission -->
+                    <!-- start unsubscribe -->
+                    <tr>
+                        <td align="center" bgcolor="#e9ecef" class="table-24-color">
+                            <p>Lorem Ipsum is simply dummy text of the printing <a href="#" target="_blank">and typesetting</a> industry.</p>
+                            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                        </td>
+                    </tr>
+                    <!-- end unsubscribe -->
+                    </table>
+                </td>
+            </tr>
+            <!-- end footer -->
+        </table>
+    <!-- end body -->
+    </div>
+</body>
