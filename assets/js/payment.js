9b403d06662b209ea4b9a5e1d8d495954f0798af
NLAKHDARI <nagib.lakhdari@ynov.com>
2022-12-22 12:13:25 +0100

    debug & comment-page

---
+++ assets/js/payment.js
@@ -0,0 +1,20 @@
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+    })
+
+function errorPayment(){
+    swal("Error occured !");
+}
+function validrPayment(){
+    swal({
+        title: 'Congratulations!',
+        text: 'You entered the correct answer',
+        type: 'success',
+        button: {
+          text: "Continue",
+          value: true,
+          visible: true,
+          className: "btn btn-primary"
+        }
+      })
+}
