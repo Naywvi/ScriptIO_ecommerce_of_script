5f7d36eea6661214025dbe32d110d70494932e33
NLAKHDARI <nagib.lakhdari@ynov.com>
2022-12-23 00:52:30 +0100

    update header

--- view/_header.php
+++ view/_header.php
@@ -21,9 +21,6 @@
                         <li class="nav-item active ">
                             <a class="nav-link nav-color" href="/">Home<span class="sr-only">(current)</span></a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link nav-color" href="/best-seller" tabindex="-1" aria-disabled="true">Best Seller</a>
-                        </li>
                         <li class="nav-item dropdown ">
                             <a class="nav-link nav-color dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     Items
@@ -33,37 +30,37 @@
                                     <h6 class="dropdown-header nav-color bold">FREE</h6>
                                     <i class='fas fa-euro-sign navicon'></i>
                                 </div>
-                                <a class="dropdown-item nav-trans nav-trans nav-color" href="#">Free</a>
+                                <a class="dropdown-item nav-trans nav-trans nav-color" href="wrap-item/?rubric=free?field=free">Free</a>
                                 <div class="dropdown-divider"></div>
                                 <div class="nav-flex">
                                     <h6 class="dropdown-header nav-color nav-color">Fun </h6>
                                     <i class='far fa-hand-lizard navicon'></i>
                                 </div>
-                                <a class="dropdown-item nav-trans nav-color" href="#">Social networks</a>
-                                <a class="dropdown-item nav-trans nav-color" href="#">Scrapping</a>
+                                <a class="dropdown-item nav-trans nav-color" href="wrap-item/?rubric=fun?field=social-networks">Social networks</a>
+                                <a class="dropdown-item nav-trans nav-color" href="wrap-item/?rubric=fun?field=scrapping">Scrapping</a>
                                 <div class="dropdown-divider"></div>
                                 <div class="nav-flex">
                                     <h6 class="dropdown-header nav-color">Automating</h6>
                                     <i class='fas fa-list navicon'></i>
                                 </div>
-                                <a class="dropdown-item nav-trans nav-color" href="#">Emailing</a>
-                                <a class="dropdown-item nav-trans nav-color" href="#">Macro</a>
-                                <a class="dropdown-item nav-trans nav-color" href="#">Web</a>
+                                <a class="dropdown-item nav-trans nav-color" href="wrap-item/?rubric=automating?field=emailing">Emailing</a>
+                                <a class="dropdown-item nav-trans nav-color" href="wrap-item/?rubric=automating?field=macro">Macro</a>
+                                <a class="dropdown-item nav-trans nav-color" href="wrap-item/?rubric=automating?field=web">Web</a>
                                 <div class="dropdown-divider"></div>
                                 <div class="nav-flex">
                                     <h6 class="dropdown-header nav-color">OS </h6>
                                     <i class='fas fa-desktop navicon'></i>
                                 </div>
-                                <a class="dropdown-item nav-trans nav-color" href="#">Windows</a>
-                                <a class="dropdown-item nav-trans nav-color" href="#">Linux</a>
-                                <a class="dropdown-item nav-trans nav-color" href="#">MacOS</a>
-                                <a class="dropdown-item nav-trans nav-color" href="#">Mobile</a>
+                                <a class="dropdown-item nav-trans nav-color" href="wrap-item/?rubric=os?field=windows">Windows</a>
+                                <a class="dropdown-item nav-trans nav-color" href="wrap-item/?rubric=os?field=linux">Linux</a>
+                                <a class="dropdown-item nav-trans nav-color" href="wrap-item/?rubric=os?field=macos">MacOS</a>
+                                <a class="dropdown-item nav-trans nav-color" href="wrap-item/?rubric=os?field=mobile">Mobile</a>
                                 <div class="dropdown-divider"></div>
                                 <div class="nav-flex">
                                     <h6 class="dropdown-header nav-color">Other </h6>
                                     <i class='fas fa-glasses navicon'></i>
                                 </div>
-                                <a class="dropdown-item nav-trans nav-color" href="#">Other</a>
+                                <a class="dropdown-item nav-trans nav-color" href="wrap-item/?rubric=other?field=other">Other</a>
                             </div>
                         </li>
                         <li class="nav-item">
